8099f3ddc2d83d4225427283a72a8639700e6a92
josuerushanika <josuerushanika@gmail.com>
2023-07-30 21:31:49 +0200

    Update files

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -5,11 +5,13 @@ function Header() {
   return (
     <header>
         <div className='wrapper'>
+            <div className='cta'>
            <p className='course-name'>Sound Design Masterclass</p>
            <h1>Learn the Art of Sound Design</h1>
            <a href='#' className='demo-btn'>
             Demo Lesson
            </a>
+        </div>
         </div>
     </header>
   )
