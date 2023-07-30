54d49e103eeba123a62e5aaa3ffb4f753994f373
josuerushanika <josuerushanika@gmail.com>
2023-07-31 00:07:23 +0200

    Update Navbar

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,26 @@ import "./Navbar.css"
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+      <a href='#' className='logo'> sound<span className='red'></span>DZign</a>
+      <ul>
+        <li>
+          <a href='#'>Course Details</a>
+        </li>
+
+        <li>
+          <a href='#'>About</a>
+        </li>
+
+        <li>
+          <a href='#'>Blog</a>
+        </li>
+
+        <li>
+          <a href='#'>Testimonials</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
