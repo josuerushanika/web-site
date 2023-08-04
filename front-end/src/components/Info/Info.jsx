faa6b6a4837e7653ac0a6731549157ab21ed3a20
josuerushanika <josuerushanika@gmail.com>
2023-08-04 17:22:26 +0200

    Update files

--- front-end/src/components/Info/Info.jsx
+++ front-end/src/components/Info/Info.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Info.css'
+import { studentIcon, videoIcon } from '../../assets'
 
 function Info() {
   return (
-    <div>infos</div>
+    <section id='info' className='dark-gray'>
+      <div className='wrapper'>
+        <div className='content-container'>
+           <div className='info-content'>
+             <img src={studentIcon}/>
+             <div className='amount'>23,000</div>
+             <div className='type'>Student</div>
+           </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
