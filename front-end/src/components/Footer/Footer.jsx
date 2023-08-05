9e6393f7b412f528962399ca1e156bd9dce1fbcd
josuerushanika <josuerushanika@gmail.com>
2023-08-05 16:36:23 +0200

    Update files

--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
 import "./Footer.css"
+import { instagramLogo, twitterLogo, facebookLogo } from '../../assets'
 
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='black'>
+      <div className='wrapper'>
+        <div className='content-container'>
+          <div className='links'>
+            <a href='#' className='logo'>
+              sound<span className='red'>DZign</span>
+            </a>
+
+            <div className='social-icons'>
+               <a href='#'>
+                <img src={facebookLogo} alt=''></img>
+               </a>
+               <a href='#'>
+                <img src={twitterLogo} alt=''></img>
+               </a>
+               <a href='#'>
+                <img src={instagramLogo} alt=''></img>
+               </a>
+            </div>
+
+            <div className='copyright'>
+              This Website is designed by GTCoding c 2023
+            </div>
+          </div>
+
+          <div className='links'>
+           <h3>Quick Links</h3>
+           <ul>
+            <li><a href="#">Blog</a></li>
+                      <li><a href="#">Privacy Policy</a></li>
+           </ul>
+          </div>
+          <div className='links'>
+               <h3>Contact Us</h3>
+               <ul>
+                <li>
+                  <a href="#">contact@email.com</a>
+                </li>
+                <li>
+                  <a href="#">+243977308096</a>
+                </li>
+               </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
