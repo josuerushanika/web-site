b19dab0c66ac85837bd7fe28a63be57127b46634
josuerushanika <josuerushanika@gmail.com>
2023-08-05 02:11:53 +0200

    Update css

--- front-end/src/components/Testimonials/Testimonials.jsx
+++ front-end/src/components/Testimonials/Testimonials.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { testimonial1, testimonial2, testimonial3 } from '../../assets'
 
 function Testimonials() {
   return (
-    <div>Testimonials</div>
+    <section id="testimonials" className='dark-gray'>
+      <div className='wrapper'>
+        <h2>What is our student Say?</h2>
+      </div>
+
+      <div className='content-container'>
+        <div className='testimonial'>
+          <img src={testimonial1} alt="" />
+          <div className='reviewer-details'>
+            <div className='name'>Peter Adams</div>
+            <div className='company-name'>Google</div>
+            <div className='review'>This is a great course. I got to learn a lot</div>
+          </div>
+        </div>
+
+        <div className='testimonial'>
+          <img src={testimonial2} alt="" />
+          <div className='reviewer-details'>
+            <div className='name'>Peter Adams</div>
+            <div className='company-name'>Google</div>
+            <div className='review'>This is a great course. I got to learn a lot</div>
+          </div>
+        </div>
+
+
+        <div className='testimonial'>
+          <img src={testimonial3} alt="" />
+          <div className='reviewer-details'>
+            <div className='name'>Peter Adams</div>
+            <div className='company-name'>Google</div>
+            <div className='review'>This is a great course. I got to learn a lot</div>
+          </div>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
