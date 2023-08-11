5c159e961d92143a935ed74c8759e4632847c5d7
josuerushanika <josuerushanika@gmail.com>
2023-08-11 13:42:36 +0200

    Fix some issues

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -5,16 +5,16 @@ import Navbar from "../Navbar/Navbar"
 function Header() {
   return (
     <header>
-        <div className='wrapper'>
-            <Navbar />
-            <div className='cta'>
-           <p className='course-name'>Sound Design Masterclass</p>
-           <h1>Learn the Art of Sound Design</h1>
-           <a href='#' className='demo-btn'>
+      <div className='wrapper'>
+        <Navbar />
+        <div className='cta'>
+          <p className='course-name'>Sound Design Masterclass</p>
+          <h1>Learn the Art of Sound Design</h1>
+          <a href='#' className='demo-btn'>
             Demo Lesson
-           </a>
+          </a>
         </div>
-        </div>
+      </div>
     </header>
   )
 }
--- front-end/src/components/Info/Info.jsx
+++ front-end/src/components/Info/Info.jsx
@@ -7,19 +7,19 @@ function Info() {
     <section id='info' className='dark-gray'>
       <div className='wrapper'>
         <div className='content-container'>
-           <div className='info-content'>
-             <img src={studentIcon}/>
-             <div className='amount'>23,000</div>
-             <div className='type'>Student</div>
-           </div>
+          <div className='info-content'>
+            <img src={studentIcon} />
+            <div className='amount'>23,000</div>
+            <div className='type'>Student</div>
+          </div>
         </div>
 
         <div className='content-container'>
-           <div className='info-content'>
-             <img src={videoIcon}/>
-             <div className='amount'>26 Hrs</div>
-             <div className='type'>Video Content</div>
-           </div>
+          <div className='info-content'>
+            <img src={videoIcon} />
+            <div className='amount'>26 Hrs</div>
+            <div className='type'>Video Content</div>
+          </div>
         </div>
         <img className='bg-element-2' src={bgElement2} />
       </div>
